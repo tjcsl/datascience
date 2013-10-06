515de3b9d45c530b996c49230fd1fcd0dbb64d85
Peter Foley <pefoley2@verizon.net>
2013-10-06 13:10:00 -0400

    more ip stuff

--- hard/ch.py
+++ hard/ch.py
@@ -1,7 +1,9 @@
 d = open('challenge_file', 'rb').read()
 import string
-ips = [str(ord(a)) for a in d if a in string.ascii_letters]
+#ips = [str(ord(a)) for a in d if a in string.ascii_letters]
+ips = [str(int(x,16)) for x in '8F FB DA EA F0 D0 A2 92'.split()]
 #ips = [str(ord(c)) for c in d if ord(c) > 31 and ord(c) < 127]
+print(ips)
 
 for x in range(0,len(ips),4):
     print('.'.join(ips[x:x+4]))
